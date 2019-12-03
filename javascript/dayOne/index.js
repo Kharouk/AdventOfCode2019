a5636e3efc93c04acd12f128383c8114e2dc64c8
Alex Kharouk <akharoukpp@gmail.com>
2019-12-03 21:16:51 +0000

    solve individual fuel/mass challennges

---
+++ javascript/dayOne/index.js
@@ -0,0 +1,5 @@
+const fuelDetector = input => Math.floor(input / 3) - 2;
+const totalFuelCalculator = input => {};
+
+module.exports = fuelDetector;
+module.exports = totalFuelCalculator;
--- javascript/dayOne/index.test.js
+++ javascript/dayOne/index.test.js
@@ -1,5 +1,14 @@
 const fuelCalculator = require("./index");
 
-it("is able to deduce fuel from the given mass", () => {
-  expect(fuelCalculator(500)).toEqual(164);
+describe("Advent of Code Day One", () => {
+  it("is able to deduce fuel from the given mass", () => {
+    expect(fuelCalculator(12)).toEqual(2);
+    expect(fuelCalculator(14)).toEqual(2);
+    expect(fuelCalculator(1969)).toEqual(654);
+    expect(fuelCalculator(100756)).toEqual(33583);
+  });
+
+  it("is able to multiple fuel sources, find the mass, and add the numbers", () => {
+    expect(totalFuelCalculator([530, 1234, 666, 4])).toEqual(123452);
+  });
 });
